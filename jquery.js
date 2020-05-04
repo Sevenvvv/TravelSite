21c43fa638f9649eaeb5f2b144d68dc5f96c4953
Ulrika Höök <hook.ulrika@gmail.com>
2020-05-04 15:32:04 +0200

    added page and jQuery functions to show more content on click

--- jquery.js
+++ jquery.js
@@ -4,4 +4,27 @@ $(document).ready(function(){
 		$(".nav-bar").slideToggle();
 	});
 
+	$(".nature-content").hide();
+
+	$("#read-less-volcano").hide();
+	$("#read-volcano").click(function(){
+		$("#volcano-content").slideToggle();
+		$("#read-more-volcano").slideToggle();
+		$("#read-less-volcano").slideToggle();
+	});
+
+	$("#read-less-geyser").hide();
+	$("#read-geyser").click(function(){
+		$("#geyser-content").slideToggle();
+		$("#read-more-geyser").slideToggle();
+		$("#read-less-geyser").slideToggle();
+	});
+
+	$("#read-less-lagoon").hide();
+	$("#read-lagoon").click(function(){
+		$("#lagoon-content").slideToggle();
+		$("#read-more-lagoon").slideToggle();
+		$("#read-less-lagoon").slideToggle();
+	});
+
 });
