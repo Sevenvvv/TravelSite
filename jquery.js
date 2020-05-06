256660edecbbfcc4d9b0a81a84827813138ee7db
Ulrika Höök <hook.ulrika@gmail.com>
2020-05-06 09:44:58 +0200

    final touch

--- jquery.js
+++ jquery.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
+
+	/*Make navbar show when hamburger button is clicked*/
 	$(".nav-bar").hide();
 	$("#menu-toggle").click(function(){
 		$(".nav-bar").slideToggle();
 	});
 
+	/*Content and read-less-button is hidden when page loads*/
 	$(".nature-content").hide();
 	$(".read-less").hide();
 
+	/*Event when read-checkbox is clicked*/
 	$(".read").click(function(){
 		$(this).siblings(".read-more").slideToggle();
 		$(this).siblings(".read-less").slideToggle();
